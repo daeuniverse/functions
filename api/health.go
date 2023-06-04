1b4761fc54548c5fbdb61a9bd6f632ed26482dc3
yqlbu <yqlbu@bu.edu>
2023-06-04 23:58:06 +0800

    feat: refine handlers and set proper resp headers
    (cherry picked from commit 22b90af9809c9295ca30cc76f30b607cab91f6d9)

---
+++ api/health.go
@@ -0,0 +1,11 @@
+package api
+
+import (
+	"net/http"
+)
+
+func HealthcheckHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write([]byte(`{"health": "ok!"}`))
+}
